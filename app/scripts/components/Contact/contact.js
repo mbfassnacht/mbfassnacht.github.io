7305e1c1fbaeb9a65361daeed01b040e76edde9a
maximobelen <maximobelen@me.com>
2016-03-09 18:19:29 -0300

    center contact links

--- app/scripts/components/Contact/contact.js
+++ app/scripts/components/Contact/contact.js
@@ -13,31 +13,31 @@ var Contact = React.createClass({
 	  <div id="contact" className="section contact">
 	    <h2 className="title">CONTACT ME</h2>
 	    <ul className="socials container-fluid">
-	      <li className="col-xs-2 col-xs-offset-1">
+	      <li className="">
 	        <a href="mailto:maximobelen@me.com">
 	          <img src="./assets/images/contact/email.png"></img>
 	          <p>EMAIL</p>
 	        </a>
 	      </li>
-	      <li className="col-xs-2">
+	      <li className="">
 	        <a href="http://twitter.com/maximobelen">
 	          <img src="./assets/images/contact/twitter.png"></img>
 	          <p>TWITTER</p>
 	        </a>
 	      </li>
-	      <li className="col-xs-2">
+	      <li className="">
 	        <a href="https://www.npmjs.com/~maximobelen">
 	          <img src="./assets/images/contact/npm.png"></img>
 	          <p>NPM</p>
 	        </a>
 	      </li>
-	      <li className="col-xs-2">
+	      <li className="">
 	        <a href="https://github.com/maximobelen">
 	          <img src="./assets/images/contact/github.png"></img>
 	          <p>GITHUB</p>
 	        </a>
 	      </li>
-	      <li className="col-xs-2">
+	      <li className="">
 	        <a href="https://uy.linkedin.com/in/maximobelen">
 	          <img src="./assets/images/contact/linkedin.png"></img>
 	          <p>LINKEDIN</p>
