cb8e9aab3129fbe689a5be81334357b90dd24d58
maximobelen <maximobelen@me.com>
2016-03-14 20:57:33 -0300

    adding content

--- app/scripts/components/About/about-model.js
+++ app/scripts/components/About/about-model.js
@@ -5,7 +5,7 @@ module.exports = {
 		"I started my studies in 2009, and in 2015 I graduated as Computer Engineer from Universidad Católica del Uruguay. I have been working the last 5 years in IT performing tasks as developer and analayst and also beeing the leader of a production team.",
 		"My first job was on a company called STATUM. There I gain experience in development and analysis of different software solutions. Particularly there I develop in Java and Javascript and also using SQL, so there I learned a lot about databases (queries, views, procedures, etc). After that I worked on an Argentinian enterprise called Globant, there I learn more about Java and Javascript, and also I developed skills as Team Leader.",
 		"Recently I worked for a year on Jam3 a Canadian Digital Agency performing tasks as Frontend Engineer using the most recently javascript technologies and frameworks.",
-		"Nowadays I'm looking for an opportunity in a Europe Company, to continue improving my skills and to learn and explore Europe culture and history as much as posible"
+		"Nowadays I'm looking for an opportunity in a Europe Company, to continue improving my skills and to learn and explore Europe culture and history as much as posible."
 	],
 	'phrase': "Stay Hungry, Stay Foolish",
 	'author': "Steve Jobs - Stanford 2005",
--- app/scripts/components/ProjectsGrid/projectsGrid-model.js
+++ app/scripts/components/ProjectsGrid/projectsGrid-model.js
@@ -21,8 +21,8 @@ module.exports = {
 				'name': 'Buenavianda',
 				'client': 'Buenavianda',
 				'image': '../assets/images/projects/buenavianda.png',
-				'technologies': 'Javascript, Node.js, NPM, Gulp, Sass',
-				'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. '
+				'technologies': 'Javascript, Node.js, Angular, NPM, Gulp, Sass',
+				'description': 'I worked in this project as a freelance, helping two other guys to develop the frontend side of the project. The site is completely build in Angular.'
 			},
 			{
 				'name': 'Mustang Customizer',
@@ -54,14 +54,14 @@ module.exports = {
 				'client': 'K12',
 				'image': '../assets/images/projects/k12.png',
 				'technologies': 'Javascript, Java, SQL',
-				'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. '
+				'description': 'I worked in this e-learning system for 2 years. I started working as a Java Developer but some months later I choose to move to frontend and since there I worked always in frontend. K12 is a hug application with many platforms, so we worked in small teams developing differents parts of the system. My last month in k12 I worked as a team leader of one of this teams.'
 			},
 			{
 				'name': 'Apia ITIL',
 				'client': 'BPS',
 				'image': '../assets/images/projects/apia.png',
-				'technologies': 'Javascript, Java, SQL',
-				'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. '
+				'technologies': 'Javascript, Java, SQL, BPM',
+				'description': 'I worked as a developer in this project for 2 years, with 2 more collegues. My role in this team was to as a developer and analyst, being one of the touch point with the client. The piece of software that we created involved all the process that an ITIL solution should have: Event Management, Access Management, Request Fulfillment, Problem Management, Incident Management.'
 			}	
 		]
 	],
