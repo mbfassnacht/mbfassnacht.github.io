a23e350c45f1c6f33c44a920b6809df810ba730b
maximobelen <maximobelen@me.com>
2016-03-16 21:45:42 -0300

    fix arrow

--- app/scripts/components/Arrow/arrow.js
+++ app/scripts/components/Arrow/arrow.js
@@ -11,15 +11,25 @@ var Arrow = React.createClass({
 		this.scroller =  new ScrollManager();
 		this.container = ReactDOM.findDOMNode(this);
   		TweenMax.to(this.container, 0.4, {delay:0.8, autoAlpha: 1});
+  		this.image = this.container.getElementsByClassName('image-arrow')[0];
+
 	},
 
 	handleMouseDown: function(){
 		this.scroller.scrollTo({element: document.body, to: window.innerHeight, duration: 0.8, ease:'easeOutCubic'});
 	},
 
+	handleMouseEnter: function(){
+  		TweenMax.to(this.image, 0.4, {autoAlpha: 0.5});
+	},
+
+	handleMouseLeave: function(){
+  		TweenMax.to(this.image, 0.4, {autoAlpha: 1});
+	},
+
 	render: function() {
 		return (
-			<div id="arrow" className="white circle-button" onMouseDown={this.handleMouseDown}>
+			<div id="arrow" className="white circle-button" onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave} onMouseDown={this.handleMouseDown}>
 				<img className="image-arrow" src={model.arrow}></img>
 			</div>
 		);
