320e21a594bc16e14f660dbd31a1d320744e4c09
maximobelen <maximobelen@me.com>
2017-05-04 15:54:18 -0300

    add new events for project overlay show

--- app/scripts/components/Project/project.js
+++ app/scripts/components/Project/project.js
@@ -8,7 +8,7 @@ var Project = React.createClass({
 	componentDidMount: function() {
 		this.container = ReactDOM.findDOMNode(this);
 		this.hoverContainer = this.container.getElementsByClassName('hover-container')[0];
-		
+		this.overlayShown = false;
 		this.name = this.container.getElementsByClassName('project-name')[0];
 		this.client = this.container.getElementsByClassName('project-client')[0];
 		this.description = this.container.getElementsByClassName('project-description')[0];
@@ -27,6 +27,14 @@ var Project = React.createClass({
   		this.container.style.width = this.container.parentNode.clientWidth / 2;
 	},
 
+	handleClick: function() {
+  		if (this.overlayShown) {
+  			this.handleMouseLeave();
+  		} else {
+  			this.handleMouseEnter();
+  		}
+	},
+
   	handleMouseEnter: function() {
   		if (!this.isAnimating) {
 			this.isAnimating = true;
@@ -34,10 +42,17 @@ var Project = React.createClass({
 			TweenMax.to(this.hoverContainer, 0.4, {autoAlpha: 1, ease: Expo.easeOut});
 			TweenMax.staggerFromTo(this.animateArray, 0.4, {autoAlpha: 0, y:15}, {autoAlpha: 1, y:0, ease: Expo.easeOut}, 0.2, function(){
 				this.isAnimating = false;
+				this.overlayShown = true;
 				if(this.requestedAnimateOut) {
 					this.handleMouseLeave();
 				}
 			}.bind(this));
+		}
+	},
+
+	handleMouseMove: function() {
+		if (!this.overlayShown) {
+			this.handleMouseEnter();
 		}
 	},
 
@@ -49,6 +64,7 @@ var Project = React.createClass({
 			TweenMax.staggerFromTo(this.reverseAnimateArray, 0.4, {autoAlpha: 1, y:0}, {autoAlpha: 0, y:15, ease: Expo.easeOut}, 0.2);
 			TweenMax.to(this.hoverContainer, 0.4, {delay: 0.6, autoAlpha: 0, ease: Expo.easeOut, onComplete: function(){
 				this.isAnimating = false;
+				this.overlayShown = false;
 			}.bind(this)});		
 		} else {
 			this.requestedAnimateOut = true;
@@ -59,7 +75,7 @@ var Project = React.createClass({
 	render: function() {  
 
 		return (
-		  	<div className="project" onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave}>
+		  	<div className="project" onClick={this.handleClick} onMouseMove={this.handleMouseMove} onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave}>
 		  		<div className="content-container">
 			  		<div className="image-container">
 			  			<img className="image" src={this.props.image}></img>
