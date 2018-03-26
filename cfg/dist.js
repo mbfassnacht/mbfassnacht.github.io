eca70875c44ea5b479d80c7b3bea0e3781168de8
Maximo Belen <maximo.fassnacht@exozet.com>
2018-03-26 00:31:34 -0300

    add canvas

--- cfg/dist.js
+++ cfg/dist.js
@@ -10,7 +10,9 @@ let defaultSettings = require('./defaults');
 let BowerWebpackPlugin = require('bower-webpack-plugin');
 
 let config = Object.assign({}, baseConfig, {
-  entry: path.join(__dirname, '../src/index'),
+  entry: [
+    './src/scripts/index'
+  ],
   cache: false,
   devtool: 'sourcemap',
   plugins: [
--- src/scripts/components/DescriptionCanvas/descriptionCanvas.js
+++ src/scripts/components/DescriptionCanvas/descriptionCanvas.js
@@ -2,7 +2,7 @@ require('styles/components/DescriptionCanvas/descriptionCanvas.scss');
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {PerspectiveCamera, WebGLRenderer, Scene, Texture, MeshBasicMaterial, Particle} from 'three';
+import * as THREE from 'three';
 import Avatar from '../Avatar/avatar';
 
 var model = require('./descriptionCanvas-model');
@@ -11,7 +11,6 @@ class DescriptionCanvas extends React.Component {
 
 	componentDidMount() {
 		this.container = ReactDOM.findDOMNode(this);
-		this.particles= [];
 		this.initCanvas();
 		this.scrollListener = this.onScroll.bind(this);
 		window.addEventListener('scroll', this.scrollListener, true);
@@ -26,69 +25,86 @@ class DescriptionCanvas extends React.Component {
 	}
 
 	initCanvas () {
-		var domElement = ReactDOM.findDOMNode(this.refs.canvas);
-		this.camera = new PerspectiveCamera(80, (this.container.clientWidth - 30) / this.container.clientHeight, 1, 4000 );
+		this.mouseX = 0;
+		this.mouseY = 0;
+		this.windowHalfX = window.innerWidth / 2;
+		this.windowHalfY = window.innerHeight / 2;
+		this.init();
+		this.animate();
+
+	}
+
+	init() {
+		this.camera = new THREE.PerspectiveCamera( 55, window.innerWidth / window.innerHeight, 2, 2000 );
 		this.camera.position.z = 1000;
+		this.scene = new THREE.Scene();
+		this.scene.fog = new THREE.FogExp2( 0x000000, 0.001 );
+		this.geometry = new THREE.Geometry();
+		this.sprite = new THREE.TextureLoader().load( "../images/disc.png" );
+		for ( var i = 0; i < 10000; i ++ ) {
+			var vertex = new THREE.Vector3();
+			vertex.x = 2000 * Math.random() - 1000;
+			vertex.y = 2000 * Math.random() - 1000;
+			vertex.z = 2000 * Math.random() - 1000;
+			this.geometry.vertices.push( vertex );
+		}
+		this.material = new THREE.PointsMaterial( { size: 35, sizeAttenuation: false, map: this.sprite, alphaTest: 0.5, transparent: true } );
+		this.material.color.setHSL( 1.0, 0.3, 0.7 );
+		this.particles = new THREE.Points( this.geometry, this.material );
+		this.scene.add( this.particles );
+		//
+		this.renderer = new THREE.WebGLRenderer();
+		this.renderer.setPixelRatio( window.devicePixelRatio );
+		this.renderer.setSize( window.innerWidth, window.innerHeight );
+		this.container.appendChild( this.renderer.domElement );
 
-		this.scene = new Scene();
-		this.scene.add(this.camera);
-
-		this.renderer = new WebGLRenderer( { antialias: true } );
-		this.renderer.setSize( this.container.clientWidth - 30, this.container.clientHeight );
-		domElement.appendChild( this.renderer.domElement );
-		this.makeParticles();
-
-		setInterval(this.update.bind(this), 1000/30);
-		window.addEventListener('resize', function(){
-			this.camera.aspect = (this.container.clientWidth - 30) / this.container.clientHeight;
-			this.camera.updateProjectionMatrix();
-			this.renderer.setSize( this.container.clientWidth - 30, this.container.clientHeight );
-		}.bind(this), true);
+		document.addEventListener( 'mousemove', this.onDocumentMouseMove.bind(this), false );
+		document.addEventListener( 'touchstart', this.onDocumentTouchStart.bind(this), false );
+		document.addEventListener( 'touchmove', this.onDocumentTouchMove.bind(this), false );
+		window.addEventListener( 'resize', this.onWindowResize.bind(this), false );
+	}
+	onWindowResize() {
+		this.windowHalfX = window.innerWidth / 2;
+		this.windowHalfY = window.innerHeight / 2;
+		this.camera.aspect = window.innerWidth / window.innerHeight;
+		this.camera.updateProjectionMatrix();
+		this.renderer.setSize( window.innerWidth, window.innerHeight );
 	}
 
-	update() {
-	  this.updateParticles();
-	  this.renderer.render( this.scene, this.camera );
+	onDocumentMouseMove( event ) {
+		this.mouseX = event.clientX - this.windowHalfX;
+		this.mouseY = event.clientY - this.windowHalfY;
 	}
 
- 	particleRender( context ) {
-
-	  context.beginPath();
-	  context.arc( 0, 0, 1, 0,  Math.PI * 2, true );
-	  context.fill();
-	}
-
-	updateParticles () {
-		var particle;
-		for(var i=0; i<this.particles.length; i++) {
-
-			particle = this.particles[i];
-			particle.position.z +=  5;
-			if(particle.position.z > 1000) particle.position.z-= 2000;
-
+ 	onDocumentTouchStart( event ) {
+		if ( event.touches.length == 1 ) {
+			event.preventDefault();
+			this.mouseX = event.touches[ 0 ].pageX - this.windowHalfX;
+			this.mouseY = event.touches[ 0 ].pageY - this.windowHalfY;
 		}
 	}
 
-	makeParticles() {
-		var particle, material;
-
-	   	var texture = new Texture();
-    	texture.needsUpdate = true;
-
-		for ( var zpos= -1000; zpos < 1000; zpos+=10 ) {
-
-			material = new MeshBasicMaterial( { color: 0xffffff, program: this.particleRender, opacity: 0.5 } );
-    		particle = new Particle(material);
-
-			particle.position.x = Math.random() * 1000 - 500;
-			particle.position.y = Math.random() * 1000 - 500;
-			particle.position.z = zpos;
-			particle.scale.x = particle.scale.y = 4;
-
-			this.scene.add(particle);
-			this.particles.push(particle);
+	onDocumentTouchMove( event ) {
+		if ( event.touches.length == 1 ) {
+			event.preventDefault();
+			this.mouseX = event.touches[ 0 ].pageX - this.windowHalfX;
+			this.mouseY = event.touches[ 0 ].pageY - this.windowHalfY;
 		}
+	}
 
+	animate() {
+		requestAnimationFrame( this.animate.bind(this) );
+		this.renderCanvas();
+	}
+
+	renderCanvas() {
+		var time = Date.now() * 0.00005;
+		this.camera.position.x += ( this.mouseX - this.camera.position.x ) * 0.05;
+		this.camera.position.y += ( - this.mouseY - this.camera.position.y ) * 0.05;
+		this.camera.lookAt( this.scene.position );
+		var h = ( 360 * ( 1.0 + time ) % 360 ) / 360;
+		this.material.color.setHSL( h, 0.5, 0.5 );
+		this.renderer.render( this.scene, this.camera );
 	}
 
 	render() {
